fe0a9afae4ffdb7b74cb192cca3fde714c5f4c7d
Misha Holzman <mcholzman1@gmail.com>
2018-09-20 09:54:58 -0400

    set up server and completed all instructions

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
 
+const express = require ('express');
+
+const bodyParser = require ('body-parser')
+
+const app = express()
+
+const birds = require ('./bird-app/db/birds.js')
+
+const port = 3000
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+
+
+app.get('/', (req, res) => {
+	res.send('Hello World')
+})
+
+app.get('/birds/:id.json', (req, res) => {
+	res.json(birds.find(bird => bird.id == req.params.id)
+	)
+});
+
+
+app.get('/birds', (req, res) => {
+	res.json(birds)
+});
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Bird app listening on port ${port}!`));
